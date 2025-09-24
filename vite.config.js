374e029566d114b084fe3850033da521811fe17d
bosrin <rabeya15-3891@.diuedu.bd>
2025-09-24 20:18:35 +0600

    quiz-app

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/quiz-app/',   // ⚡ repo name
+  base: '/quiz-app/', // রিপোজিটরির নাম
 })
